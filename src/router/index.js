b1562242c981622273679e8f8fecbb5c358a54c7
laifugroup <laifuygj@gmail.com>
2025-03-10 15:25:58 +0800

    paper

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PagerView from '../views/PagerView.vue'
 import SealView from '../views/SealView.vue'
-import TextTestView from '../views/TextTestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
